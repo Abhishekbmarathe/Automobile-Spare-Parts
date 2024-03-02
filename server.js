b7137edd8727ac2c2982f8463e1ceea1dc2d9767
Abhishek <abhishekbmarathe@gmail.com>
2024-03-02 19:22:54 +0530

    project files added

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+
 const app = express()
 const port = 3000
 
+
+// Body parsing middleware for handling form submissions
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// setting ejs template engine
+app.set('view engine', 'ejs')
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render("index.ejs")
 })
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
