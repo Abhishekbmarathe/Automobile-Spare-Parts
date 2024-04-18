104e8e8ad06b6be001cfe5000e754e6e96ad8332
shreyas <shreyask253@gmail.com>
2024-04-18 17:17:13 +0530

    modified

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
 
 
 const app = express()
@@ -17,29 +20,38 @@ const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+// database connection
+mongoose.connect("mongodb://127.0.0.1:27017/authenticat")
+    .then(() => {
+        console.log("MONGODB connected Successfully...");
+    })
+    .catch((e) => {
+        console.log("MONGODB connection Failed...", e);
+    })
 
 
 
 
-app.get('/', (req, res) => { 
-  res.render("index.ejs")
+app.get('/', (req, res) => {
+    res.render("index.ejs")
 })
 
 
 app.post("/parts", (req, res) => {
-  res.render("parts");
+    res.render("parts");
 })
 
-app.post("/buy", (req,res) => {
-  res.render("buy");
+app.post("/buy", (req, res) => {
+    res.render("buy");
 })
-
-
+app.post("/accessories", (req, res) => {
+    res.render("accessories");
+})
 
 
 
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
